c54e0d538ab4f143040e0b1850f8d847d2bcd42f
Christian Gravel <cjgravel@gmail.com>
2023-04-12 15:56:17 -0700

    added functions for rendering product cards in app.js / refactored index.html

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,11 +1,70 @@
 // Client facing scripts here
 
-var heartIcon = document.querySelector(".fas.fa-heart");
+// var heartIcon = document.querySelector(".fas.fa-heart");
 
-heartIcon.addEventListener("click", function() {
-  heartIcon.style.color = "red";
-});
+// heartIcon.addEventListener("click", function() {
+//   heartIcon.style.color = "red";
+// });
 
-const renderCards = function(cards) {
-  
+const renderCards = function(products) {
+  $(".cards").empty();
+  for(const product of products) {
+    const cardElement = createCardElement(product);
+    $(".cards").prepend(cardElement); 
+  }
 }
+
+const createCardElement = function(product) {
+
+  let $card = $(`
+  <div class="col-md-6 col-sm-12 col-lg-4">
+    <div class="card-wrapper">
+      <div class="card">
+        <div class="image-container">
+          <div class="square-image">
+            <img src="${product.img}" class="img-fluid">
+            <h2 class="overlay-text">${product.is_sold ? "SOLD" : ""}</h2>
+          </div>
+          <button class="favorite-btn">
+            <i class="fas fa-heart"></i>
+          </button>
+          <button class="delete-btn">
+            <i class="fa-solid fa-xmark"></i>
+          </button>
+          <button class="sold-btn">
+            <i class="fa-solid fa-tag"></i>
+          </button>
+        </div>
+        <div class="card-body">
+          <div class="d-flex justify-content-between">
+            <h5 class="card-title">${product.title}</h5>
+            <p class="card-text float-right">$${product.price}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  `);
+  return $card;
+}
+
+$(document).ready(function() {
+
+  function loadProducts() {
+
+    $.ajax({
+      url: '/api/products',
+      method: 'GET',
+      dataType: 'json',
+      success: function(products) {
+        renderCards(products);
+      },
+      error: function(err) {
+        console.error('Error fetching products:', err);
+      }
+    });
+  }
+
+  loadProducts();
+  
+});
