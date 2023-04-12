6dbf1e49433d4783537554b29d619d10a9cf076e
takuyan <takuyan@node-1w7jr9pm6zp0yrwqd6whr4e58.ipv6.telus.net>
2023-04-11 18:40:57 -0700

    Dynamic product page

--- routes/product.js
+++ routes/product.js
@@ -5,11 +5,21 @@
  * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
  */
 
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
+const productQueries = require('../db/queries/products');
 
-router.get('/product', (req, res) => {
-  res.render('product');
-}); 
+router.get("/product/:id", (req, res) => {
+  const productId = req.params.id;
+  productQueries
+    .getProductById(productId)
+    .then((product) => {
+      console.log(product)
+      res.render("product", product);
+    })
+    .catch((err) => {
+      res.status(500).json({ error: err.message });
+    });
+});
 
 module.exports = router;
