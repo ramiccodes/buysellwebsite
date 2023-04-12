44d1ddf92ae0a73659223fc448218841a8e96df7
Christian Gravel <cjgravel@gmail.com>
2023-04-12 12:30:42 -0700

    pulling new repo updates

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,11 @@
 // Client facing scripts here
+
+var heartIcon = document.querySelector(".fas.fa-heart");
+
+heartIcon.addEventListener("click", function() {
+  heartIcon.style.color = "red";
+});
+
+const renderCards = function(cards) {
+  
+}
