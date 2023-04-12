3d63ed291debb4f9fc246c705c5efbb279eef605
takuyan <takuyan@node-1w7jr9pm6zp0yrwqd6whr4e58.ipv6.telus.net>
2023-04-12 14:16:44 -0700

    Add categories to adding post + redirect when finish

--- public/scripts/create.js
+++ public/scripts/create.js
@@ -2,31 +2,6 @@
 let imgUrl = "";
 
 $(document).ready(function () {
-  $("#form").submit(function (event) {
-
-    // Bootstrap validation
-    event.preventDefault();
-    event.stopPropagation();
-    this.classList.add("was-validated");
-
-    // Adds img URL to the end of the serialized string
-    let queryString = `${$(this).serialize()}&img=${imgUrl}`;
-
-    if (this.checkValidity()) {
-      $.ajax({
-        method: "POST",
-        url: "/api/products",
-        data: queryString,
-        success: (res) => {
-          console.log(res);
-        },
-        error: (err) => {
-          console.log(err);
-        },
-      });
-    }
-  });
-
   $("#upload-image").change(function (event) {
     // Gets file from form, and formats it for POST
     file = event.target.files[0];
@@ -42,7 +17,7 @@ $(document).ready(function () {
       contentType: false,
       success: (res) => {
         // Update url for form submission
-        imgUrl = res.data.filename;
+        $("#img").val(res.data.filename);
       },
       error: () => {
         console.log("Error");
