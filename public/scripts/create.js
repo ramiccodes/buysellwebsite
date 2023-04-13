d215de1cdf53b95c4c4026c2f2e091ca96f5a546
takuyan <takuyan@node-1w7jr9pm6zp0yrwqd6whr4e58.ipv6.telus.net>
2023-04-12 17:07:05 -0700

    Authorization redirecting on create page

--- public/scripts/create.js
+++ public/scripts/create.js
@@ -8,15 +8,21 @@ function renderTextOnInput(name) {
 }
 
 $(document).ready(function () {
+  // If page reloads, it should take the values inside the input
+  $("#card-title").text($("#input-title").val());
+  $("#card-price").text("$" + $("#input-price").val());
+  $("#card-category").text($("#input-category").val());
+
+  // Listent to any inputs on page, and render
   renderTextOnInput("title");
   renderTextOnInput("price");
   renderTextOnInput("description");
-
   $("#input-category").change(function (event) {
     const text = $(this).find(":selected").val();
     $("#card-category").text(text);
   });
 
+  // Upload image on form input
   $("#upload-image").change(function (event) {
     // Gets file from form, and formats it for POST
     file = event.target.files[0];
@@ -33,7 +39,10 @@ $(document).ready(function () {
       success: (res) => {
         // Update url for form submission
         $("#img").val(res.data.filename);
-        $("#card-img").attr("src", res.data.filename);
+        $("#card-img").css(
+          "background-image",
+          "url(" + res.data.filename + ")"
+        );
       },
       error: () => {
         console.log("Error");
